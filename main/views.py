14504e55cf90a38f8b1ff177bfaf726ece78f4ca
Sirered <gikurniawan.1995@gmail.com>
2023-09-12 09:56:26 +0700

    Adding my name and class

--- main/views.py
+++ main/views.py
@@ -2,7 +2,9 @@ from django.shortcuts import render
 
 def show_main(request):
     context = {
-        'name': 'Weregarurumon',
+        'name' : 'Galih Ibrahim Kurniawan',
+        'class' : 'PBP KKI',
+        'cardname': 'Weregarurumon',
         'category': 'Blue, Lv.7 Digimon',
         'set_release': 'RB-01: Resurgence Booster',
         'amount' : 0,
