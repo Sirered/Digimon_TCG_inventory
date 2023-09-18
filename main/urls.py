185862db5ba28632ce38ad0d3d0b5b41daa4fa98
Sirered <gikurniawan.1995@gmail.com>
2023-09-18 11:19:03 +0700

    Continuing Assignment 3

--- main/urls.py
+++ main/urls.py
@@ -4,6 +4,7 @@ from main.views import show_main, create_item
 app_name = 'main'
 
 urlpatterns = [
+    path('<int:id>/', show_main, name='show_main'),
     path('', show_main, name='show_main'),
     path('create-item', create_item, name='create_item'),
 ]
