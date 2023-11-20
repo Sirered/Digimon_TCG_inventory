d104920521a267bb929e30a3cc588c29f56bd399
Sirered <gikurniawan.1995@gmail.com>
2023-11-20 17:01:23 +0700

    something

--- main/views.py
+++ main/views.py
@@ -122,7 +122,8 @@ def show_xml(request):
     return HttpResponse(serializers.serialize("xml", data), content_type="application/xml")
 
 def show_json(request):
-    data = Item.objects.filter(user = request.user)
+    print(request.user)
+    data = Item.objects.filter(user__username = request.user)
     return HttpResponse(serializers.serialize("json", data), content_type = "application/json")
 
 def show_xml_by_id(request, id):
